17c9da39b4d65a1caee175dd623353888a7f661e
Flynn <dev@flyn.ca>
2021-04-11 01:06:58 -0400

    Add command options

--- utils/dedupe-images.js
+++ utils/dedupe-images.js
@@ -10,13 +10,13 @@ setOptions({
 	// directory to walk
 	sourceDirectory: path.resolve(process.cwd(), relativeSourceDirectory),
 	// number of files to give to each thread at a time
-	hashingBatchSize: 6,
+	hashingBatchSize: commandOptions.hashBatchSize || 6,
 	// number of comparisons per thread at a time
 	comparisonBatchSize: 1,
 	// the minimum size in bytes
-	minimumByteSize: 1, // 11 * 1024,
+	minimumByteSize: 11 * 1024,
 	// the resolution to generate the image hash
-	levenDetailLevel: 24,
+	levenDetailLevel: commandOptions.detailLevel || 24,
 	// leven threshold (direct)
 	levenThreshold: 22,
 	// the maximum number of items per leven calculation
